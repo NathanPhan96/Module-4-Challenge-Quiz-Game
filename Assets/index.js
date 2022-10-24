a15b61a8f3ffc99d25c9fde4088be631220401f9
Nathan Phan <nathanphan96@yahoo.com>
2022-10-23 21:24:50 -0700

    started working on js file

---
+++ Assets/index.js
@@ -0,0 +1,7 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var highScores = document.querySelector(".High-Score");
+
+var timer;
+var timerCount;
+var highScores;
